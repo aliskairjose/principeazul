44f3f2ea56905f36a5c23d333fd104086a7d318a
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-05-22 15:53:10 -0400

    New pages

---
+++ src/views/Clients/Clients.vue
@@ -0,0 +1,15 @@
+<template>
+  <b-container fluid>
+    <h3>Clientes</h3>
+  </b-container>
+</template>
+<script>
+import { vito } from '../../config/pluginInit'
+
+export default {
+  name: 'Clients',
+  mounted () {
+    vito.index()
+  }
+}
+</script>
---
+++ src/views/Inventory/Inventory.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Inventario
+  </div>
+</template>
+<script>
+import { vito } from '../../config/pluginInit'
+
+export default {
+  name: 'Inventory',
+  mounted () {
+    vito.index()
+  }
+}
+</script>
