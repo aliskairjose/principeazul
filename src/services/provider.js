3e7dc84c1429762ff8f7dc1d1171b8b6429cdf6c
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-05-29 17:27:46 -0400

    services

--- src/services/provider.js
+++ src/services/provider.js
@@ -4,31 +4,31 @@ export default {
   async getAll () {
     const token = localStorage.getItem('access_token')
     axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
-    const res = await axios.get('/categories')
+    const res = await axios.get('/providers')
     return res.data
   },
   async getById (id) {
     const token = localStorage.getItem('access_token')
     axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
-    const res = await axios.get(`/categories/${id}`)
+    const res = await axios.get(`/providers/${id}`)
     return res.data
   },
   async create (params) {
     const token = localStorage.getItem('access_token')
     axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
-    const res = await axios.post('/categories', params)
+    const res = await axios.post('/providers', params)
     return res.data
   },
   async update (id, params) {
     const token = localStorage.getItem('access_token')
     axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
-    const res = await axios.put(`/categories/${id}`, params)
+    const res = await axios.put(`/providers/${id}`, params)
     return res.data
   },
   async delete (id) {
     const token = localStorage.getItem('access_token')
     axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
-    const res = await axios.delete(`/categories/${id}`)
+    const res = await axios.delete(`/providers/${id}`)
     return res.data
   }
 }
