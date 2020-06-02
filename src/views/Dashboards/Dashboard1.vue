b680d9e75c8db22a64eb44206788ac22bfc8ee44
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-06-02 13:06:41 -0400

    chatsr

--- src/views/Dashboards/Dashboard1.vue
+++ src/views/Dashboards/Dashboard1.vue
@@ -228,62 +228,14 @@ export default {
       ],
       chart1: {
         series: [{
-          name: 'Net Profit',
-          data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
-        }, {
-          name: 'Revenue',
+          name: 'Ganancias totales',
           data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
         }],
         chart: {
           type: 'bar',
           height: 350
         },
-        colors: ['#827af3', '#6ce6f4'],
-        plotOptions: {
-          bar: {
-            horizontal: false,
-            columnWidth: '55%',
-            endingShape: 'rounded'
-          }
-        },
-        dataLabels: {
-          enabled: false
-        },
-        stroke: {
-          show: true,
-          width: 2,
-          colors: ['transparent']
-        },
-        xaxis: {
-          categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct']
-        },
-        yaxis: {
-        },
-        fill: {
-          opacity: 1
-        },
-        tooltip: {
-          y: {
-            formatter: function (val) {
-              return '$ ' + val + ' thousands'
-            }
-          }
-        }
-      },
-      darkChart1: {
-        series: [{
-          name: 'Net Profit',
-          data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
-        }, {
-          name: 'Revenue',
-          data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
-        }],
-        chart: {
-          type: 'bar',
-          foreColor: '#8c91b6',
-          height: 350
-        },
-        colors: ['#827af3', '#6ce6f4'],
+        colors: ['#6ce6f4'],
         plotOptions: {
           bar: {
             horizontal: false,
