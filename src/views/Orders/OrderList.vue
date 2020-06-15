8a78cf707ecfad1e0e108ea38dd5913124862082
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-06-15 11:19:00 -0400

    variante

--- src/views/Orders/OrderList.vue
+++ src/views/Orders/OrderList.vue
@@ -75,7 +75,7 @@
                       <b-badge variant="light" v-if="orders.item.status === 'Confeccionado'">{{orders.item.status}}</b-badge>
                       <b-badge variant="info" v-if="orders.item.status === 'En camino a reparto'">{{orders.item.status}}</b-badge>
                       <b-badge variant="success" v-if="orders.item.status === 'Entregado'">{{orders.item.status}}</b-badge>
-                      <b-badge variant="primary" v-if="orders.item.status === 'Cancelado'">{{orders.item.status}}</b-badge>
+                      <b-badge variant="danger" v-if="orders.item.status === 'Cancelado'">{{orders.item.status}}</b-badge>
                     </template>
                     <template v-slot:cell(action)="orders">
                       <b-button
