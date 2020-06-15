ab044edeaebdc98e94d7d780a624bbb190b52784
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-06-14 22:29:24 -0400

    ajuste

--- src/services/index.js
+++ src/services/index.js
@@ -3,26 +3,10 @@ import constant from '../config/constant'
 
 let token = document.head.querySelector('meta[name="csrf-token"]') || ''
 
-const configVue = axios.create({
-  baseURL: constant.webBaseURL
-})
-
-configVue.interceptors.request.use(
-  (config) => {
-    let bearerToken = localStorage.getItem('access_token')
-
-    if (bearerToken) {
-      config.headers['Authorization'] = `Bearer ${bearerToken}`
-    }
-    config.headers['X-Requested-With'] = 'XMLHttpRequest'
-    config.headers['X-CSRF-TOKEN'] = token
-
-    return config
-  },
-
-  (error) => {
-    return Promise.reject(error)
+export default axios.create({
+  baseURL: constant.webBaseURL,
+  headers: {
+    'X-Requested-With': 'XMLHttpRequest',
+    'X-CSRF-TOKEN': token
   }
-)
-
-export default configVue
+})
