643f3e7e4dc186e56bd4718326cdd3c26e9c1275
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-06-19 09:48:13 -0400

    ajuste de altura dashboard

--- src/views/Dashboards/Dashboard1.vue
+++ src/views/Dashboards/Dashboard1.vue
@@ -9,7 +9,7 @@
                 :src="iframeUrl"
                 frameborder="0"
                 width="100%"
-                height="600"
+                height="650"
                 allowtransparency>
               </iframe>
             </div>
