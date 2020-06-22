1124141215895ae32027d91715d31e5f01dc8f83
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-06-22 11:01:02 -0400

    Dashboard Taller con Orderlist

--- src/views/Dashboards/DashboardTaller.vue
+++ src/views/Dashboards/DashboardTaller.vue
@@ -17,14 +17,25 @@
         </iq-card>
       </b-col>
     </b-row>
+    <b-row>
+      <b-col md="12">
+        <iq-card>
+          <template v-slot:body>
+            <OrderList></OrderList>
+          </template>
+        </iq-card>
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
 <script>
 import { vito } from '../../config/pluginInit'
+import OrderList from '@/views/Orders/OrderList'
 
 export default {
   name: 'DasboardTaller',
+  components: { OrderList },
   mounted () {
     vito.index()
     let jwt = require('jsonwebtoken')
