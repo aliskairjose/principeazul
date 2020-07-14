14314dae745dfa026e451b12df8581b6932fae42
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-07-14 09:11:49 -0400

    api turns

--- src/services/general.js
+++ src/services/general.js
@@ -5,16 +5,24 @@ export default {
     const res = await axios.get('/payment-methods')
     return res.data
   },
+
   async orderStatus () {
     const res = await axios.get('/order-statuses')
     return res.data
   },
+
   async reasons () {
     const res = await axios.get('/reasons')
     return res.data
   },
+
   async deliveryZones () {
     const res = await axios.get('/delivery-zones')
+    return res.data
+  },
+
+  async turns () {
+    const res = await axios.get('/turns')
     return res.data
   }
 }
