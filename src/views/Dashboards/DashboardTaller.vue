804d03e6c24f33ef9816ec90841e28728636dec6
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-07-17 11:07:09 -0400

    Inclusion de filtros

--- src/views/Dashboards/DashboardTaller.vue
+++ src/views/Dashboards/DashboardTaller.vue
@@ -19,13 +19,78 @@
         <iq-card>
           <template v-slot:body>
             <!-- <OrderList></OrderList> -->
-           <b-row>
-             <b-col md="4" v-for="order in orders" :key="order.id">
-               <OrderCard
-                :order="order"
-                :statuses="statuses"
-                @status-change="updateStatus($event)"></OrderCard>
-             </b-col>
+            <b-row align-h="between" align-v="center">
+              <b-col md="3">
+                <b-form-group
+                    label="Filtro"
+                    label-cols-sm="3"
+                    label-align-sm="right"
+                    label-size="sm"
+                    label-for="filterInput"
+                    class="mb-0 pt-3">
+                    <b-input-group size="sm">
+                      <b-form-input
+                        v-model="filter"
+                        type="search"
+                        id="filterInput"
+                        placeholder="Escriba para buscar">
+                      </b-form-input>
+                      <b-input-group-append>
+                        <b-button :disabled="!filter" @click="filter = ''">Limpiar</b-button>
+                      </b-input-group-append>
+                    </b-input-group>
+                </b-form-group>
+              </b-col>
+              <b-col md="2">
+                  <b-form-group class="mb-0 pt-3">
+                    <b-form-select
+                      v-model="selectedPersonalize"
+                      id="types"
+                      size="sm"
+                      :options="perOptions"
+                      @change="onChangePersonalized"
+                    ></b-form-select>
+                  </b-form-group>
+              </b-col>
+              <b-col md="2">
+                  <b-form-group class="mb-0 pt-3">
+                    <b-form-select
+                      v-model="selectedType"
+                      id="types"
+                      size="sm"
+                      :options="typeOptions"
+                      @change="onSelectedChange"
+                    ></b-form-select>
+                  </b-form-group>
+              </b-col>
+              <b-col md="2">
+                  <b-form-group label="Fecha inicial:" label-for="date">
+                    <b-form-input
+                      :disabled="selectedType ? false : true"
+                      v-model="initDate"
+                      type="date"
+                      @change="onDateChange"
+                    ></b-form-input>
+                  </b-form-group>
+              </b-col>
+              <b-col md="2">
+                  <b-form-group label="Fecha final:" label-for="date">
+                    <b-form-input
+                      :disabled="selectedType ? false : true"
+                      v-model="endDate"
+                      type="date"
+                      @change="onDateChange"
+                    ></b-form-input>
+                  </b-form-group>
+              </b-col>
+              </b-row>
+            <b-row>
+            <b-col md="4" v-for="order in orders" :key="order.id">
+              <OrderCard
+               :order="order"
+               :statuses="statuses"
+               @status-change="updateStatus($event)"></OrderCard>
+            </b-col>
            </b-row>
           </template>
         </iq-card>
@@ -74,10 +139,23 @@ export default {
   },
   data () {
     return {
+      selectedType: null,
+      selectedPersonalize: null,
       loading: false,
       orders: '',
       iframeUrl: '',
-      statuses: []
+      statuses: [],
+      typeOptions: [
+        { value: null, text: 'Rango de fecha' },
+        { value: 1, text: 'Fecha de creación' },
+        { value: 2, text: 'Fecha de entrega' }
+      ],
+      perOptions: [
+        { value: null, text: 'Tipo de orden' },
+        { value: 2, text: 'Todos' },
+        { value: 1, text: 'Personalizada' },
+        { value: 0, text: 'Standard' }
+      ]
     }
   },
   methods: {
