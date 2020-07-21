63d1a0d0140d30588f10344ea0b5c1a6d0d04d37
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-07-21 14:26:48 -0400

    Ajuste de data y formato

--- src/views/Dashboards/Deliveries.vue
+++ src/views/Dashboards/Deliveries.vue
@@ -7,7 +7,15 @@
             <b-col md="12" class="text-center spinner" v-if="loading">
               <b-spinner variant="primary" type="grow" label="Spinning"></b-spinner>
             </b-col>
-            <div class="d-flex align-items-center justify-content-between"></div>
+            <b-row>
+              <b-col md="4" v-for="order in orders" :key="order.id">
+              <DeliveryCard
+                :order="order"
+                :statuses="statuses"
+                @status-change="updateStatus($event)"
+              ></DeliveryCard>
+            </b-col>
+            </b-row>
           </template>
         </iq-card>
       </b-col>
@@ -18,21 +26,36 @@
 <script>
 import { vito } from '../../config/pluginInit'
 import orderService from '@/services/order'
+import DeliveryCard from '@/views/Dashboards/OrderCard/DeliveryCard'
 import moment from 'moment'
 
 export default {
   name: 'Deliveries',
+  components: { DeliveryCard },
   mounted () {
     vito.index()
+    orderService.orderStatuses()
+      .then(response => {
+        const object = response.data
+        for (const iterator of object) {
+          let status = {}
+          status.value = iterator
+          status.text = iterator
+          this.statuses.push(status)
+        }
+      })
+      .catch(error => { console.log(error) })
 
     const date = new Date()
     const formatDate = moment(String(date)).format('YYYY-MM-DD')
+    this.loadData()
     this.loadData(`delivery_init_date=${formatDate}&delivery_end_date=${formatDate}`)
   },
   data () {
     return {
-      order: Array,
-      loading: false
+      orders: [],
+      loading: false,
+      statuses: []
     }
   },
   methods: {
@@ -40,10 +63,18 @@ export default {
       this.loading = true
       orderService.getAll(params).then(response => {
         this.orders = [...response.data]
+        console.log(response.data)
       })
         .catch(() => {})
         .finally(() => { this.loading = false })
     }
+  },
+  updateStatus ($event) {
+    this.loading = true
+    orderService.updateSatus($event.id, $event.status)
+      .then(() => { })
+      .catch(() => { })
+      .finally(() => { this.loading = false })
   }
 }
 </script>
---
+++ src/views/Dashboards/OrderCard/DeliveryCard.vue
@@ -0,0 +1,109 @@
+<template>
+  <b-container fluid>
+    <b-row class="card-order">
+      <b-col md="12" style="box-shadow: 5px 5px 5px var(--iq-secondary-light) !important;">
+        <iq-card>
+          <template v-slot:body>
+            <b-row>
+              <b-col md="12" class="d-flex justify-content-between">
+                <h4>#{{order.id}}</h4>
+              </b-col>
+            </b-row>
+            <b-row>
+              <b-col md="12">
+                <p>Persona que recibe: {{ order.addressee }}</p>
+                <p>Día de entrega: {{ order.delivery_date | formatWeekDate }}</p>
+                <p v-if="order.client.phone">Teléfono: {{ order.client.phone }}</p>
+                <p>Direccón: {{ order.delivery_address }}</p>
+              </b-col>
+            </b-row>
+            <b-row class="my-4" v-for="product in order.products" :key="product.id">
+              <b-col md="4">
+                <b-img
+                  v-viewer="{movable: false}"
+                  center
+                  rounded="circle"
+                  :src="product.product.image ? product.product.image : require(`@/assets/images/no-image.png`)"
+                  class="image"
+                ></b-img>
+              </b-col>
+              <b-col md="8">
+                <b-row>
+                  <b-col md="12">
+                    <h5>{{product.name}}</h5>
+                  </b-col>
+                </b-row>
+                <b-row>
+                  <b-col md="12">
+                    <label
+                      class="text-muted"
+                      style="max-width: 200px; font-style: italic; font-size: 11px"
+                      for v-if="product.note">
+                      "{{product.note | capitalize}}"
+                    </label>
+                  </b-col>
+                </b-row>
+              </b-col>
+            </b-row>
+            <b-row class="mt-3 mb-0 d-flex justify-content-between">
+              <b-col md="6">
+                <b-form-select
+                  v-model="order.status"
+                  :options="statuses"
+                  @change="onStatusChange(order.id, $event)">
+                </b-form-select>
+              </b-col>
+              <b-col lg="4" md="12" sm="12">
+                <b-badge
+                  variant="primary"
+                  class="px-3"
+                  v-if="order.personalizedRequired">
+                  Personalizado
+                </b-badge>
+              </b-col>
+            </b-row>
+          </template>
+        </iq-card>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import { vito } from '../../../config/pluginInit'
+
+export default {
+  name: 'DeliveryCard',
+  props: {
+    order: { type: Object },
+    statuses: { type: Array }
+  },
+  mounted () {
+    vito.index()
+  },
+  methods: {
+    onStatusChange (id, $event) {
+      const status = $event
+      this.$emit('status-change', { id, status })
+    }
+  }
+}
+</script>
+
+<style>
+.card-order {
+  margin-bottom: 10px;
+  border-radius: 5px;
+  border: solid thin var(--iq-secondary-light);
+}
+
+.iq-card {
+  margin-top: 10px;
+  margin-bottom: 0px !important;
+}
+
+p {
+  font-size: 13px
+}
+
+</style>
