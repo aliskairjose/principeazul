6dcf704b9b43a31c82fd2ccb8d48cfea9084db94
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-09-03 16:12:15 -0400

    feat: Delivery Zone Name

--- src/views/Orders/OrderUpdate.vue
+++ src/views/Orders/OrderUpdate.vue
@@ -5,6 +5,9 @@
     <div v-for="item in order.products" :key="item.id" class="text-center">
       {{ item.name }}
     </div>
+    <div>
+      Zona de entrega: {{ deliveryZone }}
+    </div>
     <UpdateForm :order="order"></UpdateForm>
   </div>
 </template>
@@ -12,33 +15,39 @@
 <script>
 import UpdateForm from './Forms/UpdateForm'
 import orderService from '@/services/order'
+import commonServices from '@/services/general'
 
 export default {
   name: 'OrderUpdate',
   components: { UpdateForm },
   data: () => ({
     loading: true,
-    order: {
-      delivery_date: '',
-      delivery_address: '',
-      addressee: '',
-      dedication: '',
-      signature: '',
-      phone_number: null
-    }
+    deliveryZones: [],
+    deliveryZone: '',
+    order: {}
   }),
-  mounted () {
-    const id = this.$route.params.id
-    orderService.getById(id)
-      .then(response => {
-        this.order = response.data
-        console.log(this.order)
-        const value = this.order.delivery_date
-        this.order.delivery_date = value.slice(0, value.indexOf(' '))
-      })
-      .catch(error => { console.log(error) })
-      .finally(() => { this.loading = false })
+  created () {
+    commonServices.deliveryZones().then(response => {
+      this.deliveryZones = [...response.data]
+      const id = this.$route.params.id
+      this.loadData(id)
+    })
   },
-  methods: { }
+  mounted () {
+  },
+  methods: {
+    loadData (id) {
+      orderService.getById(id)
+        .then(response => {
+          this.order = { ...response.data }
+          const zones = this.deliveryZones.filter(x => x.id === this.order.delivery_zone_id)
+          this.deliveryZone = zones[0].name
+          const value = this.order.delivery_date
+          this.order.delivery_date = value.slice(0, value.indexOf(' '))
+        })
+        .catch(error => { console.log(error) })
+        .finally(() => { this.loading = false })
+    }
+  }
 }
 </script>
