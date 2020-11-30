b7d01c132185f6b6aae231b2e3124887c15b3879
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-11-30 14:02:13 -0400

    style: ajuste de impresion

--- src/components/Order/OrderDetailComponent.vue
+++ src/components/Order/OrderDetailComponent.vue
@@ -35,89 +35,97 @@
                 </div>
               </div>
             </b-col>
-            <div class="row">
-              <div class="col-md-3 div-left">
-                <p class="text-uppercase font-weight-bolder">Cliente:</p>
-                <p class="text-uppercase">Teléfono:</p>
-                <p class="text-uppercase">Tipo de compra:</p>
-                <p class="text-uppercase">Total a pagar</p>
-                <p class="text-uppercase">Pagado:</p>
-                <p class="text-uppercase">Saldo Pendiente:</p>
-                <p class="text-uppercase">Método de pago:</p>
-              </div>
-              <div class="col-md-3 div-rigth">
-                <p class="font-weight-bolder">{{ data.client.name }}</p>
-                <p>{{ data.client.phone }}</p>
-                <p>{{ data.type }}</p>
-                <p>{{ data.total | money }}</p>
-              </div>
-            </div>
+            <b-container>
+              <b-row>
+                <b-col>
+                  <p class="text-uppercase font-weight-bolder">Cliente:</p>
+                  <p class="text-uppercase">Teléfono:</p>
+                  <p class="text-uppercase">Tipo de compra:</p>
+                  <p class="text-uppercase">Total a pagar</p>
+                  <p class="text-uppercase">Pagado:</p>
+                  <p class="text-uppercase">Saldo Pendiente:</p>
+                  <p class="text-uppercase">Método de pago:</p>
+                </b-col>
+                <b-col>
+                  <p class="font-weight-bolder">{{ data.client.name }}</p>
+                  <p>{{ data.client.phone }}</p>
+                  <p>{{ data.type }}</p>
+                  <p>{{ data.total | money }}</p>
+                </b-col>
+              </b-row>
+            </b-container>
             <hr />
             <!-- Detalle de los productos -->
-            <b-row>
-              <b-col md="6">
-                <p class="text-uppercase">Motivo:</p>
-                <p class="text-uppercase">
-                  Mensaje para la tarjeta dedicatoria:
-                </p>
-              </b-col>
-              <b-col md="6">
-                <p>{{ data.reason }}</p>
-                <p>{{ data.dedication }}</p>
-              </b-col>
-            </b-row>
-            <b-row v-for="(p, index) in data.products" :key="index">
-              <b-col md="8">
-                <h4>{{ p.name | capitalize }} {{ p.price | money }}</h4>
-                <div v-for="(a, index) in p.additionals" :key="index">
-                  <h6 v-if="a.type === 'extra'" class="ml-4 text-black-50">
-                    {{ a.name | capitalize }} x {{ a.quantity }}
-                  </h6>
-                </div>
-                <b-row class="mt-2">
-                  <b-col md="6">
-                    <p class="text-uppercase">Nota de taller:</p>
-                    <p class="text-uppercase">Nota de diseño:</p>
-                    <p class="text-uppercase">Personalización de texto:</p>
-                  </b-col>
-                  <b-col md="6">
-                    <p>{{ p.note }}</p>
-                    <p>{{ p.note_design }}</p>
-                    <p>{{ p.personalized_text }}</p>
-                  </b-col>
-                </b-row>
-              </b-col>
-              <b-col md="4">
-                <b-img
-                  v-viewer="{ movable: false }"
-                  center
-                  rounded="circle"
-                  :src="
-                    p.product.image
-                      ? p.product.image
-                      : require(`@/assets/images/no-image.png`)
-                  "
-                  class="image-list"
-                ></b-img>
-                <div v-for="(a, i) in p.additionals" :key="i">
+            <b-container>
+              <b-row>
+                <b-col>
+                  <p class="text-uppercase">Motivo:</p>
+                  <p class="text-uppercase">
+                    Mensaje para la tarjeta dedicatoria:
+                  </p>
+                </b-col>
+                <b-col>
+                  <p>{{ data.reason }}</p>
+                  <p>{{ data.dedication }}</p>
+                </b-col>
+              </b-row>
+            </b-container>
+            <b-container>
+              <b-row v-for="(p, index) in data.products" :key="index">
+                <b-col>
+                  <h4>{{ p.name | capitalize }} {{ p.price | money }}</h4>
+                  <div v-for="(a, index) in p.additionals" :key="index">
+                    <h6 v-if="a.type === 'extra'" class="ml-4 text-black-50">
+                      {{ a.name | capitalize }} x {{ a.quantity }}
+                    </h6>
+                  </div>
+                  <b-container>
+                    <b-row class="mt-2">
+                      <b-col>
+                        <p class="text-uppercase">Nota de taller:</p>
+                        <p class="text-uppercase">Nota de diseño:</p>
+                        <p class="text-uppercase">Personalización de texto:</p>
+                      </b-col>
+                      <b-col>
+                        <p>{{ p.note }}</p>
+                        <p>{{ p.note_design }}</p>
+                        <p>{{ p.personalized_text }}</p>
+                      </b-col>
+                    </b-row>
+                  </b-container>
+                </b-col>
+                <b-col>
                   <b-img
-                    v-if="a.type === 'extra'"
                     v-viewer="{ movable: false }"
                     center
                     rounded="circle"
                     :src="
-                      a.product.image
-                        ? a.product.image
+                      p.product.image
+                        ? p.product.image
                         : require(`@/assets/images/no-image.png`)
                     "
                     class="image-list"
                   ></b-img>
-                </div>
-              </b-col>
-            </b-row>
+                  <div v-for="(a, i) in p.additionals" :key="i">
+                    <b-img
+                      v-if="a.type === 'extra'"
+                      v-viewer="{ movable: false }"
+                      center
+                      rounded="circle"
+                      :src="
+                        a.product.image
+                          ? a.product.image
+                          : require(`@/assets/images/no-image.png`)
+                      "
+                      class="image-list"
+                    ></b-img>
+                  </div>
+                </b-col>
+              </b-row>
+            </b-container>
             <hr />
             <!-- Datos de la entrega -->
-            <b-col md="12">
+            <b-col>
               <div class="clearfix">
                 <div class="float-left">
                   <h5>Datos de la entrega</h5>
@@ -127,30 +135,32 @@
                 </div>
               </div>
             </b-col>
-            <b-row>
-              <b-col md="4">
-                <p class="text-uppercase">Fecha de entrega:</p>
-                <p class="text-uppercase">Dirección de entrega:</p>
-                <p class="text-uppercase">Recibe:</p>
-                <p class="text-uppercase">Turno:</p>
-                <p class="text-uppercase">Teléfono:</p>
-                <p class="text-uppercase">Cantidad:</p>
-                <p class="text-uppercase">Productos:</p>
-              </b-col>
-              <b-col md="8">
-                <p>
-                  {{ data.delivery_date | formatWeekDate }}
-                </p>
-                <p>{{ data.delivery_address }}</p>
-                <p>{{ data.addressee }}</p>
-                <p>{{ data.turn }}</p>
-                <p>{{ data.phone }}</p>
-                <p>{{ data.products.length }}</p>
-                <p v-for="(p, i) in data.products" :key="i">
-                  {{ p.name | capitalize }}
-                </p>
-              </b-col>
-            </b-row>
+            <b-container>
+              <b-row>
+                <b-col>
+                  <p class="text-uppercase">Fecha de entrega:</p>
+                  <p class="text-uppercase">Dirección de entrega:</p>
+                  <p class="text-uppercase">Recibe:</p>
+                  <p class="text-uppercase">Turno:</p>
+                  <p class="text-uppercase">Teléfono:</p>
+                  <p class="text-uppercase">Cantidad:</p>
+                  <p class="text-uppercase">Productos:</p>
+                </b-col>
+                <b-col>
+                  <p>
+                    {{ data.delivery_date | formatWeekDate }}
+                  </p>
+                  <p>{{ data.delivery_address }}</p>
+                  <p>{{ data.addressee }}</p>
+                  <p>{{ data.turn }}</p>
+                  <p>{{ data.phone }}</p>
+                  <p>{{ data.products.length }}</p>
+                  <p v-for="(p, i) in data.products" :key="i">
+                    {{ p.name | capitalize }}
+                  </p>
+                </b-col>
+              </b-row>
+            </b-container>
           </template>
         </iq-card>
       </b-col>
@@ -248,14 +258,14 @@ export default {
   font-style: italic;
 }
 @media print {
-  .div-left {
-    width: 30%;
-    background-color: red;
+  .row-data {
+    display: flex;
+    flex-direction: row;
   }
   p {
     text-transform: uppercase;
   }
-  .image {
+  .image-list {
     width: 80px;
     height: auto;
   }
