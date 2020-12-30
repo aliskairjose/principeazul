cbdb28ae1481708830d4b7d2f5c95c0b2cf435d6
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-12-30 09:57:47 -0400

    ajuste

--- src/views/Dashboards/DashboardTaller.vue
+++ src/views/Dashboards/DashboardTaller.vue
@@ -253,7 +253,7 @@ export default {
         .then(() => {})
         .catch(() => { })
         .finally(() => {
-          this.loadData(``)
+          this.filterData()
           this.loading = false
         })
     },
