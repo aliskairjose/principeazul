fbd75e0807364757453b5f1b367eefedb216bbbf
Sabas Vega <sabascarloseduardo@gmail.com>
2021-07-19 16:42:09 -0500

    Fixed date params on calendar component

--- src/services/calendar.js
+++ src/services/calendar.js
@@ -2,7 +2,7 @@ import axios from './index'
 
 export default {
   async getAll (firstDay, lastDay) {
-    const res = await axios.get(`/orders?init_date=${firstDay}&end_date=${lastDay}`)
+    const res = await axios.get(`/orders?delivery_init_date=${firstDay}&delivery_end_date=${lastDay}`)
     return res.data
   }
 }
